ebe25aaf3d1b691a204f6a1e791801ac75238338
chrisphine <onyangoketta@gmail.com>
2022-10-03 19:40:30 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,24 @@
 class ItemsController < ApplicationController
+  wrap_parameters format: []
 
   def index
     items = Item.all
-    render json: items, include: :user
+    render json: items, include: :user, status: :not_found
   end
 
+  def show
+    item = Item.find_by(id: params[:id])
+    render json: item, include: :user, status: :not_found
+  end
+  
+  def create
+    item = Item.create(item_params)
+    render json: item, status: :created, include: :user
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
 end
